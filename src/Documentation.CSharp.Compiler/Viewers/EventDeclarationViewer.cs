ab8ca8fd665453f1458bba961930e0530a042d0e
Sharp0802 <sharp0802.business@gmail.com>
2022-09-21 00:56:09 +0900

    Impl event decl viewer

---
+++ src/Documentation.CSharp.Compiler/Viewers/EventDeclarationViewer.cs
@@ -0,0 +1,75 @@
+﻿using Documentation.CSharp.Compiler.Primitives;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Text;
+
+namespace Documentation.CSharp.Compiler.Viewers;
+
+public class EventDeclarationViewer : DeclarationViewer
+{
+    public override DeclarationKind Kind => DeclarationKind.Event;
+
+    public override bool IsSupported(SyntaxNode syntax, ISymbol symbol) => syntax is VariableDeclaratorSyntax or EventDeclarationSyntax && symbol is IEventSymbol;
+
+    public override string? View(SemanticModel semantic, SyntaxNode syntax)
+    {
+        if (syntax is EventDeclarationSyntax eventSyntax)
+        {
+            if (semantic.GetDeclaredSymbol(eventSyntax) is not IEventSymbol symbol)
+                return null;
+
+            var builder = new StringBuilder();
+
+            var attributes = ViewAttributes(symbol.GetAttributes(), "");
+            if (!string.IsNullOrEmpty(attributes))
+                builder.Append(attributes).Append('\n');
+
+            var accessibility = ViewAccessibility(symbol.DeclaredAccessibility);
+            if (!string.IsNullOrEmpty(accessibility))
+                builder.Append(accessibility).Append(' ');
+
+            var modifier = ViewModifiers(eventSyntax);
+            if (!string.IsNullOrEmpty(modifier))
+                builder.Append(modifier).Append(' ');
+
+            return builder
+                .Append("event ")
+                .Append(symbol.Type.ToDisplayString())
+                .Append(' ')
+                .Append(symbol.Name)
+                .Append(" { add; remove; }")
+                .ToString();
+        }
+        else if (syntax is VariableDeclaratorSyntax fieldSyntax)
+        {
+            if (semantic.GetDeclaredSymbol(fieldSyntax) is not IEventSymbol symbol)
+                return null;
+
+            var builder = new StringBuilder();
+
+            var attributes = ViewAttributes(symbol.GetAttributes(), "");
+            if (!string.IsNullOrEmpty(attributes))
+                builder.Append(attributes).Append('\n');
+
+            var accessibility = ViewAccessibility(symbol.DeclaredAccessibility);
+            if (!string.IsNullOrEmpty(accessibility))
+                builder.Append(accessibility).Append(' ');
+
+            var modifier = ViewModifiers(symbol);
+            if (!string.IsNullOrEmpty(modifier))
+                builder.Append(modifier).Append(' ');
+
+            return builder
+                .Append("event ")
+                .Append(symbol.Type.ToDisplayString())
+                .Append(' ')
+                .Append(symbol.Name)
+                .Append(" { add; remove; }")
+                .ToString();
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
