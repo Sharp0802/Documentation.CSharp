d7a1e422816d2da4ea2e0faf480f307b8742aeac
Sharp0802 <sharp0802.business@gmail.com>
2022-09-26 01:00:37 +0900

    Impl tree view node component

---
+++ src/Documentation.CSharp.Web/Primitives/TreeNode.cs
@@ -0,0 +1,15 @@
+﻿namespace Documentation.CSharp.Web.Primitives;
+
+public class TreeNode
+{
+    public TreeNode(string? data, string text, TreeNode[] children)
+    {
+        Data = data;
+        Text = text;
+        Children = children;
+    }
+
+    public string? Data { get; }
+    public string Text { get; }
+    public TreeNode[] Children { get; }
+}
