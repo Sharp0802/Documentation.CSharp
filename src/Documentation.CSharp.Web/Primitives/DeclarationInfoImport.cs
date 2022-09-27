1ed8318ae3eef326b86c220ed9691dd93a6c093c
Sharp0802 <sharp0802.business@gmail.com>
2022-09-28 01:06:29 +0900

    Upload primitive

---
+++ src/Documentation.CSharp.Web/Primitives/DeclarationInfoImport.cs
@@ -0,0 +1,15 @@
+﻿namespace Documentation.CSharp.Web.Primitives;
+
+public class DeclarationInfoImport
+{
+    public string? Title { get; set; }
+    public string? Declaration { get; set; }
+    public int Kind { get; set; }
+    public string? Documentation { get; set; }
+    public bool IsDeclared { get; set; }
+
+    public DeclarationInfoImport[] Methods { get; set; } = null!;
+    public DeclarationInfoImport[] Events { get; set; } = null!;
+    public DeclarationInfoImport[] Properties { get; set; } = null!;
+    public DeclarationInfoImport[] Fields { get; set; } = null!;
+}
