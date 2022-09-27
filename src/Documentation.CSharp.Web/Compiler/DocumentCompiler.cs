ce737130be47087dea24ac5d3991a4e0fed6defa
Sharp0802 <sharp0802.business@gmail.com>
2022-09-28 01:06:38 +0900

    Work on progress...

---
+++ src/Documentation.CSharp.Web/Compiler/DocumentCompiler.cs
@@ -0,0 +1,9 @@
+﻿namespace Documentation.CSharp.Web.Compiler;
+
+public static class DocumentCompiler
+{
+    public static string ToMarkdown(string xml)
+    {
+        throw new NotImplementedException(); 
+    }
+}
