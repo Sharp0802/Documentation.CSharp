d86aa1f9d97819901c406da6f8af77b3da1d90be
Sharp0802 <sharp0802.business@gmail.com>
2022-10-01 06:31:13 +0100

    Update command line service; Work on progress...

--- src/Documentation.CSharp/Services/CommandLineService.cs
+++ src/Documentation.CSharp/Services/CommandLineService.cs
@@ -11,39 +11,31 @@ using System.CommandLine.Parsing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Documentation.CSharp.Commands;
 
 namespace Documentation.CSharp.Services;
 
 internal class CommandLineService : BackgroundService
 {
+    public static string[] Args { get; set; } = Array.Empty<string>();
+    
     private IHostApplicationLifetime Lifetime { get; }
-    private string[]? Args { get; }
     private ILogger<CommandLineService> Logger { get; }
+    private CommandRegistrar CommandRegistrar { get; }
 
-    public CommandLineService(IHostApplicationLifetime lifetime, IConfiguration configuration, ILogger<CommandLineService> logger)
+    public CommandLineService(
+        IHostApplicationLifetime lifetime,
+        ILogger<CommandLineService> logger,
+        CommandRegistrar commandRegistrar)
     {
         Lifetime = lifetime;
-        Args = configuration.GetValue<string[]>("args");
         Logger = logger;
-    }
-
-    private static void FileInfoValidator(OptionResult result)
-    {
-        var file = result.GetValueOrDefault<FileInfo?>();
-        if (file is null || !file.Exists)
-            result.ErrorMessage = "File does not exist.";
-    }
-
-    private static void BuildableValidator(OptionResult result)
-    {
-        var file = result.GetValueOrDefault<FileInfo?>();
-        if (file?.Extension is not (".sln" or ".csproj"))
-            result.ErrorMessage = "File is not buildable. File must be Project Solution or C# Project";
+        CommandRegistrar = commandRegistrar;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        if (Args is null)
+        if (Args.Length <= 0)
         {
             Logger.Log(LogLevel.Critical, "Cannot load given arguments. Terminate program...");
             Lifetime.StopApplication();
@@ -51,81 +43,7 @@ internal class CommandLineService : BackgroundService
         else
         {
             var root = new RootCommand("a documentation generator for C#.");
-            {
-                var targetOption = new Option<FileInfo?>(
-                    name: "--target",
-                    description: "The .sln or .csproj to build documentation.");
-                targetOption.AddAlias("-t");
-                targetOption.AddValidator(FileInfoValidator);
-                targetOption.AddValidator(BuildableValidator);
-
-                var settingsOption = new Option<FileInfo?>(
-                    name: "--settings",
-                    description: "The configuration file.");
-                settingsOption.AddAlias("-s");
-                settingsOption.AddValidator(FileInfoValidator);
-
-                var configOption = new Option<string?>(
-                    name: "--configuration",
-                    description: "The build configuration. Usually one of 'Debug' or 'Release'. Can be inherited from the configuration file.");
-                configOption.AddAlias("-c");
-                configOption.AddCompletions("Debug", "Release");
-
-                var platformOption = new Option<string?>(
-                    name: "--platform",
-                    description: "The build target platform. Usually one of 'x64' or 'x86'. Can be inherited from the configuration file.");
-                platformOption.AddAlias("-p");
-                platformOption.AddCompletions("x64", "x86");
-
-                var build = new Command("build", "Build xml documentation for .sln or .csproj")
-            {
-                targetOption,
-                settingsOption,
-                configOption,
-                platformOption
-            };
-                build.AddAlias("b");
-                build.SetHandler(async (target, settings, config, platform) =>
-                {
-                    if (string.Equals(".sln", target.Extension))
-                    {
-                        var msbuild = MSBuildWorkspace.Create();
-
-                        var sln = await msbuild.OpenSolutionAsync(target.FullName, new ConsoleLogger(LoggerVerbosity.Detailed));
-
-                        var leaves = sln.Projects.ToList();
-                        foreach (var project in sln.Projects)
-                        {
-                            for (var i = 0; i < leaves.Count; ++i)
-                            {
-                                if (project.ProjectReferences.Any(reference => reference.ProjectId == leaves[i].Id))
-                                    leaves.RemoveAt(i);
-                            }
-                        }
-
-                        if (leaves.Count <= 0)
-                            Logger.Log(LogLevel.Warning, "No documentated assembly founded. No document will be builded.");
-                        else
-                            Logger.Log(LogLevel.Information, $"{leaves.Count} documented project(s) found.");
-
-                        foreach (var leaf in leaves)
-                        {
-                            if (!leaf.TryGetCompilation(out var compilation))
-                                continue;
-                        }
-                    }
-                    else if (string.Equals(".csproj", target.Extension))
-                    {
-                        var msbuild = MSBuildWorkspace.Create();
-
-                        var csproj = await msbuild.OpenProjectAsync(target.FullName, new ConsoleLogger(LoggerVerbosity.Detailed));
-                        if (!csproj.TryGetCompilation(out var compilation))
-                            ; // TODO : handle failed
-                    }
-                }, targetOption, settingsOption, configOption, platformOption);
-
-                root.AddCommand(build);
-            }
+            
         }
     }
 }
