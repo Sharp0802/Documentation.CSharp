05bebbcfbbdbdf23cf84c9a6fd6132e79ce14849
Sharp0802 <sharp0802.business@gmail.com>
2022-10-10 00:13:58 +0900

    Modernize class: DeclarationInfo

---
+++ src/Documentation.CSharp/Compiler/Primitives/DeclarationInfo.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.CodeAnalysis;
+
+namespace Documentation.CSharp.Compiler.Primitives;
+
+public class DeclarationInfo
+{
+    public DeclarationInfo(
+        ISymbol symbol, 
+        string title, 
+        string assemblyFile, 
+        DeclarationKind kind, 
+        string declaration, 
+        DeclarationInfo[] methods, 
+        DeclarationInfo[] events, 
+        DeclarationInfo[] properties, 
+        DeclarationInfo[] fields)
+    {
+        Symbol = symbol;
+        Title = title;
+        Declaration = declaration;
+        AssemblyFile = assemblyFile;
+        Kind = kind;
+        Id = Symbol.GetDocumentationCommentId();
+        Documentation = Symbol.GetDocumentationCommentXml(expandIncludes: true);
+        IsDeclared = Symbol.DeclaringSyntaxReferences.Length > 0;
+        
+        Methods = methods;
+        Events = events;
+        Properties = properties;
+        Fields = fields;
+    }
+
+    [JsonIgnore]
+    public ISymbol? Symbol { get; set; }
+
+    public string Title { get; set; }
+    public string AssemblyFile { get; set; }
+    public string Declaration { get; set; }
+    public DeclarationKind Kind { get; set; }
+    public string? Id { get; set; }
+    public string? Documentation { get; set; }
+    public bool IsDeclared { get; set; }
+
+    public DeclarationInfo[] Methods { get; set; }
+    public DeclarationInfo[] Events { get; set; }
+    public DeclarationInfo[] Properties { get; set; }
+    public DeclarationInfo[] Fields { get; set; }
+}
---
+++ src/Documentation.CSharp/Compiler/Viewers/Declaration.cs
@@ -0,0 +1,81 @@
+﻿using Documentation.CSharp.Compiler.Primitives;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Documentation.CSharp.Compiler.Viewers;
+
+public static class Declaration
+{
+    static Declaration()
+    {
+        Viewers = new DeclarationViewer[]
+        {
+            new DelegateDeclarationViewer(),
+            new EventDeclarationViewer(),
+            new FieldDeclarationViewer(),
+            new MethodDeclarationViewer(),
+            new NamedTypeDeclarationViewer(),
+            new PropertyDeclarationViewer()
+        };
+    }
+
+    private static DeclarationViewer[] Viewers { get; }
+
+    private static bool HasComment(ISymbol symbol)
+    {
+        return !string.IsNullOrEmpty(symbol.GetDocumentationCommentXml());
+    }
+
+    public static bool IsSupported(SyntaxNode syntax, ISymbol symbol)
+    {
+        return Viewers.Any(v => v.IsSupported(syntax, symbol));
+    }
+
+    public static DeclarationInfo? View(string assemblyFile, SemanticModel semantic, SyntaxNode syntax)
+    {
+        var symbol = semantic.GetDeclaredSymbol(syntax);
+        if (symbol is null) return null;
+
+        var viewer = Viewers.FirstOrDefault(v => v.IsSupported(syntax, symbol));
+        if (viewer is null) return null;
+
+        var decl = viewer.View(semantic, syntax);
+        if (decl is null) return null;
+
+        var methods = new List<DeclarationInfo>();
+        var events = new List<DeclarationInfo>();
+        var properties = new List<DeclarationInfo>();
+        var fields = new List<DeclarationInfo>();
+        
+        if (symbol is INamedTypeSymbol type)
+        {
+            foreach (var child in type.GetMembers()
+                         .Where(HasComment)
+                         .Select(member => member.DeclaringSyntaxReferences.FirstOrDefault()?.GetSyntax())
+                         .Where(childSyntax => childSyntax is not null)
+                         .Select(childSyntax => View(assemblyFile, semantic, childSyntax!))
+                         .Where(child => child is not null))
+            {
+                (child!.Kind switch
+                {
+                    DeclarationKind.Method => methods,
+                    DeclarationKind.Event => events,
+                    DeclarationKind.Property => properties,
+                    DeclarationKind.Field => fields,
+                    _ => null
+                })?.Add(child);
+            }
+        }
+
+        return new DeclarationInfo(
+            symbol, 
+            $"{symbol.Name}{DeclarationViewer.ViewGenericParameters(symbol)}", 
+            assemblyFile, 
+            viewer.Kind, 
+            decl, 
+            methods.ToArray(), 
+            events.ToArray(), 
+            properties.ToArray(), 
+            fields.ToArray());
+    }
+}
